73fc3278afc79321ad512931987b328f9961a6c4
Mihouk <arbi_14@mail.ru>
2024-06-16 00:02:28 +0300

    add

--- src/widget.py
+++ src/widget.py
@@ -1,26 +1,27 @@
 from masks import get_mask_account
 from masks import get_mask_card_number
+from datetime import datetime
 
 
-def mask_account_card(cards_number:[str]) -> list[str]:
-    ''' Функция которая маскирует номер карты и счета'''
+def mask_account_card(cards_number: str) -> str:
+    """Функция которая маскирует номер карты и счета."""
+
+    if "Счет" in cards_number:
+        mask_account = f"Счет {get_mask_account(cards_number[:])}"
+        return mask_account
+    else:
+        card = get_mask_card_number(cards_number[-16:])
+        mask_card = cards_number.replace(cards_number[-16:], card)
+        return mask_card
 
 
-    for i in cards_number:
-        if i == 'Счет':
-            return get_mask_account(cards_number)
-        else:
-            card = get_mask_card_number(cards_number[-16:])
-            mask_card = cards_number.replace(cards_number[-16:],card)
-            return mask_card
+def get_data(data: str) -> str:
+    """Функция которая возврашает дату"""
+
+    d = datetime.strptime(data, format("%Y-%m-%dT%H:%M:%S.%f"))
+    return d.strftime("%d.%m.%Y")
 
 
-if __name__ == '__main__':
-    print(mask_account_card('Maestro 1596837868705199'))
-
-
-
-
-
-
-
+if __name__ == "__main__":
+    print(mask_account_card("Visa Platinum 8990922113665229"))
+    print(get_data("2018-07-11T02:26:18.671407"))
