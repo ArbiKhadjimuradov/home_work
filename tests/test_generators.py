167a167888dcb2a7e10f82e69dc6e4dbe8395106
Mihouk <arbi_14@mail.ru>
2024-06-25 00:52:21 +0300

    .\tests\test_generators.py

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -7,7 +7,6 @@ def test_filter_by_currency():
     assert next(generator)["id"] == 142264268
 
 
-
 def test_transaction_descriptions():
     generator = transaction_descriptions(transactions)
     assert next(generator) == "Перевод организации"
@@ -16,4 +15,12 @@ def test_transaction_descriptions():
     assert next(generator) == "Перевод с карты на карту"
     assert next(generator) == "Перевод организации"
 
+
 def test_card_number_generator():
+    generator = card_number_generator(start=1, end=5)
+    assert next(generator) == '0000 0000 0000 0001'
+    assert next(generator) == '0000 0000 0000 0002'
+    assert next(generator) == '0000 0000 0000 0003'
+    assert next(generator) == '0000 0000 0000 0004'
+    assert next(generator) == '0000 0000 0000 0005'
+
