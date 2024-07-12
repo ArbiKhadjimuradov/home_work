5aad182cf418c23c6d61fb64cc76b225e5263278
Mihouk <arbi_14@mail.ru>
2024-07-12 20:04:35 +0300

    Home Work 12.2

--- src/masks.py
+++ src/masks.py
@@ -1,8 +1,21 @@
+import logging
+
+logger = logging.getLogger('masks')
+file_handler = logging.FileHandler('../logs/masks.log', encoding='utf-8')
+file_formatter = logging.Formatter('%(asctime)s %(filename)s %(levelname)s: %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
+
+
 def get_mask_card_number(card: str) -> str:
-    """Функция которая маскирует номер карты"""
+    """ Функция, которая маскирует номер карты"""
+
+    logger.info(f'Проверяем точно ли введенные данные карты ровны 16 символам')
     if len(card) == 16:
         return f"{card[:4]} {card[4:6]}{'*' * 2} {'*' * 4} {card[12:]}"
     else:
+        logger.error(f'Введенные данные не корректны')
         return ""
 
 
@@ -11,10 +24,13 @@ if __name__ == '__main__':
 
 
 def get_mask_account(acc_number: str) -> str:
-    """Функция которая возврашает маску счета"""
+    """Функция, которая возврашает маску счета"""
+
+    logger.info(f'Проверяем точно ли введенные номера счета ровны 20 символам')
     if len(acc_number) == 20:
         return f"{'*' * 2}{acc_number[-4:]}"
     else:
+        logger.error(f'Введенные данные не корректны')
         return ""
 
 
