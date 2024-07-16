1d72bb8683781bfeee9be9a656ab6ed1b8fc1931
Mihouk <arbi_14@mail.ru>
2024-07-16 23:10:26 +0300

    Fix HW_13.1

--- tests/test_financial.py
+++ tests/test_financial.py
@@ -1,22 +1,20 @@
-import unittest
-from unittest.mock import patch
-import pandas as pd
 from src.financial import open_csv_data, open_excel_data
-from typing import Any
-
-transaction = ({
-    'id': 650703,
-    'state': 'EXECUTED',
-    'date': '2023-09-05T11:30:32Z',
-    'amount': '16210',
-    'currency_name': 'Sol',
-    'currency_code': 'PEN',
-    'from': 'Счет 58803664561298323391',
-    'to': 'Счет 39745660563456619397',
-    'description': 'Перевод организации'})
+from unittest.mock import patch, mock_open
+from tests.conftest import test_info_xlcx, test_info_csv
 
 
-@patch("requests.get")
-def get_info_csv_object(mock_get: Any) -> Any:
-    mock_get.return_value.csv.return_value = transaction
-    assert open_csv_data() == transaction.get('from'), transaction.get('to')
+def test_get_info_transaction(test_info_csv, test_info_xlcx):
+    info_csv = list(open_csv_data("../data/transactions.csv"))
+    assert info_csv[0] == test_info_csv
+
+    info_xlsx = list(open_excel_data("../data/transactions_excel.xlsx"))
+    assert info_xlsx[0] == test_info_xlcx
+
+
+@patch("builtins.open", new_callable=mock_open, read_data="data")
+def test_get_info_transactions_csv_xlsx(mock_file):
+    assert open("../Data/test_transactions_csv.csv").read() == "data"
+    mock_file.assert_called_with("../Data/test_transactions_csv.csv")
+
+    assert open("../data/test_transactions_excel.xlsx").read() == "data"
+    mock_file.assert_called_with("../data/test_transactions_excel.xlsx")
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,6 +1,7 @@
 from src.masks import get_mask_account, get_mask_card_number
 from typing import Any
 
+
 def test_get_mask_account() -> Any:
     assert get_mask_account("73654108430135874305") == "**4305"
 
